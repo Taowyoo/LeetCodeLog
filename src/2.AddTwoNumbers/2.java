38a240ab511c893947dada58e913ed3d5a2486ed
Yuxiang <cao.yux@northeastern.edu>
2021-06-16 16:46:21 -0700

    :recycle: refactor: problem 2 java

--- src/2.AddTwoNumbers/2.java
+++ src/2.AddTwoNumbers/2.java
@@ -1,32 +1,26 @@
 /*
- * File: 2.java
- * File Created: 2021-01-04 20:28:56 -08:00
- * Author: Taowyoo (caoyxsh@outlook.com)
- * Brief: https://leetcode.com/problems/add-two-numbers/solution/
- * -----
- * Last Modified: 2021-01-04 21:06:22 -08:00
- * Modified By: Taowyoo (caoyxsh@outlook.com>)
- * -----
- * Copyright 2020 - 2021
+ * @Author: Nick Cao
+ * @Date: 2021-03-25 12:42:36
+ * @LastEditTime: 2021-06-06 17:19:42
+ * @LastEditors: Nick Cao
+ * @Description: 
+ * @FilePath: \LeetCodeLog\src\2.AddTwoNumbers\2.java
  */
-
 
 public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
     ListNode dummyHead = new ListNode(0);
-    ListNode p = l1, q = l2, curr = dummyHead;
+    ListNode curr = dummyHead;
     int carry = 0;
-    while (p != null || q != null) {
-        int x = (p != null) ? p.val : 0;
-        int y = (q != null) ? q.val : 0;
+    while (l1 != null || l2 != null || carry != 0) {
+        int x = (l1 != null) ? l1.val : 0;
+        int y = (l2 != null) ? l2.val : 0;
         int sum = carry + x + y;
         carry = sum / 10;
         curr.next = new ListNode(sum % 10);
         curr = curr.next;
-        if (p != null) p = p.next;
-        if (q != null) q = q.next;
+        if (l1 != null) l1 = l1.next;
+        if (l2 != null) l2 = l2.next;
     }
-    if (carry > 0) {
-        curr.next = new ListNode(carry);
-    }
+    
     return dummyHead.next;
 }
